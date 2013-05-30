754a728839298578c4b541fc4b11e8ceb0f031ab
Thomas Jakobi <thomas.jakobi@partout.info>
2013-05-30 11:35:23 +0200

    Added code to send a tweet.

--- twitx.snippet.php
+++ twitx.snippet.php
@@ -34,11 +34,14 @@ $twitter_access_token = isset($twitter_access_token) ? $twitter_access_token : F
 $twitter_access_token_secret = isset($twitter_access_token_secret) ? $twitter_access_token_secret : FALSE;
 
 // Other options
+$mode = isset($mode) ? $mode : 'timeline';
 $limit = isset($limit) ? $limit : 5;
 $twitTpl = isset($twitTpl) ? $twitTpl : '@FILE:assets/snippets/twitx/templates/twitTpl.html';
+$tweetedTpl = isset($tweetedTpl) ? $tweetedTpl : '@FILE:assets/snippets/twitx/templates/tweetedTpl.html';
 $timeline = isset($timeline) ? $timeline : 'user_timeline';
 $cache = isset($cache) ? $cache : 7200;
 $screen_name = isset($screen_name) ? $screen_name : '';
+$tweet = isset($tweet) ? $tweet : '';
 $include_rts = (isset($include_rts) && (!$include_rts)) ? 0 : 1;
 $outputSeparator = isset($outputSeparator) ? $outputSeparator : "\r\n";
 $toPlaceholder = isset($toPlaceholder) ? $toPlaceholder : '';
@@ -63,97 +66,131 @@ if (!$twitter_consumer_key || !$twitter_consumer_secret || !$twitter_access_toke
 	if (!function_exists('curl_init')) {
 		$output[] = "<strong>TwitX Error:</strong> cURL functions do not exist, cannot continue.";
 	} else {
-		// Try loading the data from cache first
-		$myCache = new evoCache('TwitX', $screen_name . '_' . $timeline);
-
-		if ($myCache->isExpired()) {
-			// Load the TwitterOAuth lib required if not exists
-			// Create new twitteroauth
-			$twitteroauth = new TwitterOAuth($twitter_consumer_key, $twitter_consumer_secret, $twitter_access_token, $twitter_access_token_secret);
-
-			// We want to use JSON format
-			$twitteroauth->format = 'json';
-			$twitteroauth->decode_json = FALSE;
-
-			// Request statuses with optinal parameters
-			$options = array(
-				'count' => $limit + 1,
-				'include_rts' => $include_rts
-			);
-			// If we have a screen_name, pass this to Twitter API
-			if ($screen_name != '') {
-				$options['screen_name'] = $screen_name;
-			}
-			// If we are viewing favourites or regular statuses
-			if ($timeline != 'favorites') {
-				$timeline = 'statuses/' . $timeline;
-			}
-			$json = $twitteroauth->get($timeline, $options);
-
-			// No errors? Save to Cache
-			if (!isset($json->error)) {
-				$myCache->write($json, $cache);
-			}
-		} else {
-			// read cached data
-			$json = $myCache->read();
-		}
-
-		// Decode this now that we have used it above in the cache
-		$json = json_decode($json);
-
-		// If there any errors from Twitter, output them...
-		if (isset($json->error)) {
-			$output[] = "<strong>TwitX Error:</strong> Could not load TwitX. Twitter responded with the error '" . $json->error . "'.";
-		} else {
-
-			$parser = new evoChunkie($twitTpl);
-			// For each result, output it
-			foreach ($json as $j) {
-
-				// Get placerholder values
-				$placeholders = array(
-					'created_at' => $j->created_at,
-					'source' => $j->source,
-					'id' => $j->id,
-					'id_str' => $j->id_str,
-					'text' => $j->text,
-					'name' => $j->user->name,
-					'screen_name' => $j->user->screen_name,
-					'profile_image_url' => $j->user->profile_image_url,
-					'location' => $j->user->location,
-					'url' => $j->user->url,
-					'description' => $j->user->description,
-				);
-				// If this is a retweet, create placeholders for this too
-				if (isset($j->retweeted_status)) {
-					$placeholders = array_merge($placeholders, array(
-						'retweet_count' => $j->retweeted_status->retweet_count,
-						'retweet_created_at' => $j->retweeted_status->created_at,
-						'retweet_source' => $j->retweeted_status->source,
-						'retweet_id' => $j->retweeted_status->id,
-						'retweet_id_str' => $j->retweeted_status->id_str,
-						'retweet_text' => $j->retweeted_status->text,
-						'retweet_name' => $j->retweeted_status->user->name,
-						'retweet_screen_name' => $j->retweeted_status->user->screen_name,
-						'retweet_profile_image_url' => $j->retweeted_status->user->profile_image_url,
-						'retweet_location' => $j->retweeted_status->user->location,
-						'retweet_url' => $j->retweeted_status->user->url,
-						'retweet_description' => $j->retweeted_status->user->description,
-							)
-					);
+		switch ($mode) {
+			case 'tweet':
+				if ($screen_name == '') {
+					$output[] = "<strong>TwitX Error:</strong> No Twitter screen name set for tweeting.";
+					break;
 				}
-				$parser->CreateVars($placeholders);
-				// Parse chunk passing values
-				$output[] = $parser->Render();
-			}
 
-			// Added option to output to placeholder
-			if ($toPlaceholder != '') {
-				$modx->setPlaceholder($toPlaceholder, implode($outputSeparator, $output));
-				$output = array();
-				$outputSeparator = '';
-			}
+				// Create new twitteroauth
+				$twitteroauth = new TwitterOAuth($twitter_consumer_key, $twitter_consumer_secret, $twitter_access_token, $twitter_access_token_secret);
+
+				// We want to use JSON format
+				$twitteroauth->format = 'json';
+				$twitteroauth->decode_json = FALSE;
+
+				$options = array(
+					'screen_name' => $screen_name,
+					'text' => urlencode(substr($modx->stripTags($tweet), 0, 140))
+				);
+				$json = $twitteroauth->post('direct_messages/new.json', $options);
+
+				$json = json_decode($json, TRUE);
+
+				if (isset($json->error)) {
+					$output[] = "<strong>TwitX Error:</strong> Could not send the Tweet. Twitter responded with the error '" . $json->error . "'.";
+				} else {
+					$parser = new evoChunkie($tweetedTpl);
+					$parser->CreateVars($json);
+					$output[] = $parser->Render();
+				}
+
+				break;
+			case 'timeline':
+			default:
+				// Try loading the data from cache first
+				$myCache = new evoCache('TwitX', $screen_name . '_' . $timeline);
+
+				if ($myCache->isExpired()) {
+					// Load the TwitterOAuth lib required if not exists
+					// Create new twitteroauth
+					$twitteroauth = new TwitterOAuth($twitter_consumer_key, $twitter_consumer_secret, $twitter_access_token, $twitter_access_token_secret);
+
+					// We want to use JSON format
+					$twitteroauth->format = 'json';
+					$twitteroauth->decode_json = FALSE;
+
+					// Request statuses with optinal parameters
+					$options = array(
+						'count' => $limit + 1,
+						'include_rts' => $include_rts
+					);
+					// If we have a screen_name, pass this to Twitter API
+					if ($screen_name != '') {
+						$options['screen_name'] = $screen_name;
+					}
+					// If we are viewing favourites or regular statuses
+					if ($timeline != 'favorites') {
+						$timeline = 'statuses/' . $timeline;
+					}
+					$json = $twitteroauth->get($timeline, $options);
+
+					// No errors? Save to Cache
+					if (!isset($json->error)) {
+						$myCache->write($json, $cache);
+					}
+				} else {
+					// read cached data
+					$json = $myCache->read();
+				}
+
+				// Decode this now that we have used it above in the cache
+				$json = json_decode($json);
+
+				// If there any errors from Twitter, output them...
+				if (isset($json->error)) {
+					$output[] = "<strong>TwitX Error:</strong> Could not load TwitX. Twitter responded with the error '" . $json->error . "'.";
+				} else {
+
+					$parser = new evoChunkie($twitTpl);
+					// For each result, output it
+					foreach ($json as $j) {
+
+						// Get placerholder values
+						$placeholders = array(
+							'created_at' => $j->created_at,
+							'source' => $j->source,
+							'id' => $j->id,
+							'id_str' => $j->id_str,
+							'text' => $j->text,
+							'name' => $j->user->name,
+							'screen_name' => $j->user->screen_name,
+							'profile_image_url' => $j->user->profile_image_url,
+							'location' => $j->user->location,
+							'url' => $j->user->url,
+							'description' => $j->user->description,
+						);
+						// If this is a retweet, create placeholders for this too
+						if (isset($j->retweeted_status)) {
+							$placeholders = array_merge($placeholders, array(
+								'retweet_count' => $j->retweeted_status->retweet_count,
+								'retweet_created_at' => $j->retweeted_status->created_at,
+								'retweet_source' => $j->retweeted_status->source,
+								'retweet_id' => $j->retweeted_status->id,
+								'retweet_id_str' => $j->retweeted_status->id_str,
+								'retweet_text' => $j->retweeted_status->text,
+								'retweet_name' => $j->retweeted_status->user->name,
+								'retweet_screen_name' => $j->retweeted_status->user->screen_name,
+								'retweet_profile_image_url' => $j->retweeted_status->user->profile_image_url,
+								'retweet_location' => $j->retweeted_status->user->location,
+								'retweet_url' => $j->retweeted_status->user->url,
+								'retweet_description' => $j->retweeted_status->user->description,
+									)
+							);
+						}
+						$parser->CreateVars($placeholders);
+						// Parse chunk passing values
+						$output[] = $parser->Render();
+					}
+
+					// Added option to output to placeholder
+					if ($toPlaceholder != '') {
+						$modx->setPlaceholder($toPlaceholder, implode($outputSeparator, $output));
+						$output = array();
+						$outputSeparator = '';
+					}
+				}
 		}
 	}
 }
