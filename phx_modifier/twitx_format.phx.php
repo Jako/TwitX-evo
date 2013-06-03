a61320990a55b80db5c2663ed7ae3a682d61bb15
Thomas Jakobi <thomas.jakobi@partout.info>
2013-06-03 10:14:07 +0200

    Bug in twitx_format modifier

--- phx_modifier/twitx_format.phx.php
+++ phx_modifier/twitx_format.phx.php
@@ -12,14 +12,14 @@
  * TwitX is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * MODX Evolution Port of TwitterXFormat
- * @author Thomas Jakobi <thomas.jakobi@partout.info> 
+ * @author Thomas Jakobi <thomas.jakobi@partout.info>
  * @version 0.7
  *
  * TwitterXFormat author: Stewart Orr @ Qodo Ltd <stewart@qodo.co.uk>
  */
-$search = array('|(http://[^ ]+)|', '|@([\\\\w_]+)|', '|#([A-Za-z0-9-_]+)|');
+$search = array('!(http://[^ ]+)!', '!@([\w_]+)!', '!#([A-Za-z0-9-_]+)!');
 $replace = array('<a href="$1" target="_blank" rel="nofollow">$1</a>', '<a href="http://twitter.com/$1" target="_blank" rel="nofollow">@$1</a>', '<a href="http://search.twitter.com/search?q=%23$1" target="_blank" rel="nofollow">#$1</a>');
 return preg_replace($search, $replace, $output);
 ?>
